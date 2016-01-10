f6484ea012917c518b352f37db6eda6578abcbc4
Benito Palacios Sánchez <benito356@gmail.com>
2016-01-10 02:04:45 +0100

    Nino: Add dummy file for post-parsers

--- ninokuni/Script/Script.cs
+++ ninokuni/Script/Script.cs
@@ -66,6 +66,11 @@ namespace Ninokuni.Script
 			);
 
 			temp.Dispose();
+
+            // Add dummy file to be able to trigger post-xml parser like Spreadsheets.
+            File.AddFile(new GameFile(
+                "script",
+                new DataStream(new System.IO.MemoryStream(), 0, 0)));
 		}
 
 		public override void Write(DataStream strOut)
